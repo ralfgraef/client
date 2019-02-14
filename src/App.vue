f62fea619c6bb6c434cf813675b91cfe2d2316d7
ralfgraef <graef.ralf@gmail.com>
2019-02-14 21:06:00 +0100

    create simpleupload component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  
+  <div class="section">
+    <div class="container">
+      <simple-upload />
+    </div>
+  </div>
 </template>
 
 <script>
+import SimpleUpload from './SimpleUpload.vue';
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    SimpleUpload
+  }
 }
 </script>
 
